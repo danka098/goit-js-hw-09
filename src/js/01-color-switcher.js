a693b5c40dc0136fbce8fd43914e5bca74fc137e
Danka Proszkiewicz <danka.proszk@gmail.com>
2023-04-10 17:29:48 +0200

    first task update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
+let timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
